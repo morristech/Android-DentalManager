3f3125cd41d6837f8b85edfd37aa573ea4c9f941
Wade Morris <wade@globalkinetic.com>
2014-09-01 20:54:17 +0200

    Initial commit

---
+++ app/src/main/java/za/co/morristech/dentalmanager/app/DentalManagerApp.java
@@ -0,0 +1,16 @@
+package za.co.morristech.dentalmanager.app;
+
+import android.app.Application;
+
+/**
+ * Created by wademorris on 2014/08/25.
+ */
+public class DentalManagerApp extends Application {
+
+    private static final String TAG = DentalManagerApp.class.getName();
+
+    public void onCreate() {
+        super.onCreate();
+        
+    }
+}
---
+++ app/src/main/java/za/co/morristech/dentalmanager/app/common/BaseActivity.java
@@ -0,0 +1,22 @@
+package za.co.morristech.dentalmanager.app.common;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import za.co.morristech.dentalmanager.app.DentalManagerApp;
+
+/**
+ * Created by wademorris on 2014/08/25.
+ */
+public class BaseActivity extends FragmentActivity {
+
+    private static final String TAG = BaseActivity.class.getName();
+
+    private DentalManagerApp mApplication;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
